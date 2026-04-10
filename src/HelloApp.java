8820f89c84d033557060b549187ccfeb82ff04de
TrishnaMalkane <trishnamalkane@gmail.com>
2026-04-10 13:12:44 +0530

    UC5-enhanced-for-loop

--- src/HelloApp.java
+++ src/HelloApp.java
@@ -1,16 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
-        StringBuilder names = new StringBuilder();
-        if (args.length > 0) {
-            for (int i = 0; i < args.length; i++) {
-                names.append(args[i]);
-                if (i != args.length - 1) {
-                    names.append(",");
-                }
-            }
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            names.append("World");
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
         }
-        System.out.println("Hello, " + names + "!");
+
     }
 }
